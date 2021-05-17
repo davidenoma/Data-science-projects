9037e27a665f53bcd07ecc1a9ac2ba37ca9e6c18
David Enoma <davidenoma@gmail.com>
2021-05-17 01:11:58 +0100

    update to the scripts

--- nnet.R
+++ nnet.R
@@ -1,32 +1,42 @@
 data <- read.csv('C:/Users/HP/Downloads/assign5.csv')
-validation_data <- read.csv("C:/Users/HP/Downloads/test-data.csv" )
 library(dplyr)
 library(tidyverse)
+#View the properties of the data 
+dim(data) 
+str(data)
+    
 #Data Preprocessing 
-#Compare the column names of the train and validation dataset.
-colnames(data) == colnames(validation_data)
-#Subset dataset and validation 
+
+#Subset dataset 
 data <- data[c(2:12,14)]
-validation_data <- validation_data[2:13]
+
+#validation_data <- validation_data[2:13]
 #convert the power, engine and mileage column to numeric:
 
+#Remove NA values
 data <- na.omit(data)
+##or replace the na
+
+
+str(data)
 #Engine
+
 for (x in 1:nrow(data)){
   z=substring(data$Engine[x],-1, 4)
   data$Engine[x] <- as.numeric(z)
-  }
+}
+
 #Power
 
 for (x in 1:nrow(data)){
   data$Power[x] <- as.numeric(str_replace_all(data$Power[x],"[a-z]"," "))
 }
+
 #mileage
 for (x in 1:nrow(data)){
   data$Mileage[x] = as.numeric(str_replace_all(data$Mileage[x],"[a-z]"," "))
 }
-#Remove columns without a price on the validation set
-validation_data_filtered <-filter(validation_data, validation_data$New_Price!="")
+
 #Convert power, engine and mileage to numeric 
 data$Power <- as.numeric(data$Power)
 data$Mileage <- as.numeric(data$Mileage)
@@ -37,15 +47,21 @@ data$Location <- as.numeric(as.factor(data$Location))
 locationsCode = c(legend="Ahmedabad,Bangalore,Chennai,Coimbatore,Delhi,Hyderabad,Jaipur,Kochi,Kolkata,Mumbai,Pune")
      
 #Feature selection 
+
 #We are selecting the variables location,
 #We are selecting  Location, Kms Driven, mileage, Engine, Power, to predict Price. 
 str(data)#To view variables
+
 # ensure the results are repeatable
 set.seed(7)
+
 # load the library
 library(mlbench)
 library(caret)
+
+#omit
 data <- na.omit(data)
+
 # calculate correlation matrix
 correlationMatrix <- cor(data[,c(2,4,8:10,12)])
 # summarize the correlation matrix
@@ -55,10 +71,12 @@ highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.5)
 # print indexes of highly correlated attributes
 print(highlyCorrelated)
 
+
+#Random FOrest Feature selectoin
 # define the control using a random forest selection function
 control <- rfeControl(functions=rfFuncs, method="cv", number=10)
 # run the RFE algorithm
-results <- rfe(data[,c(2,4,8:10)], data[,12], sizes=c(1:5), rfeControl=control)
+#results <- rfe(data[,c(2,4,8:10)], data[,12], sizes=c(1:5), rfeControl=control)
 # summarize the results
 print(results)
 # list the chosen features
@@ -66,18 +84,23 @@ predictors(results)
 # plot the results
 plot(results, type=c("g", "o"))
 
+
 #dFinal data with only selected features. 
 new_data <- data[,c(4,8:10,12)]
+
+
+
 # TRAINING AND TEST DATA
+
 inp <- sample(2, nrow(new_data), replace = TRUE, prob = c(0.7, 0.3))
 training_data <- new_data[inp==1, ]
 test_data <- new_data[inp==2, ]
+
 
 Price_max_pre_normalize = max(test_data$Price)
 Price_min_pre_normalize = min(test_data$Price) 
 
 # Min-Max Normalization
-
 
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
@@ -99,25 +122,33 @@ test_data$Power <- normalize(test_data$Power)
 #. NEURAL NETWORK
 library(neuralnet)
 #We can chage the hidden layers configuration to c(2,1)
-nn <- neuralnet(Price ~Kilometers_Driven +Mileage+Power+Engine,data=training_data, hidden=c(2,1), linear.output=TRUE, threshold=0.01)
+nn <- neuralnet(Price ~Kilometers_Driven + Mileage+Power+Engine,data=training_data, hidden=c(2,1), linear.output=TRUE, threshold=0.01)
 nn$result.matrix
 # plot our neural network 
 plot(nn, rep = 1)
+
 # Prediction
 output <- compute(nn, rep = 1, test_data)
+
+
 head(output$net.result)
 results <- data.frame(actual = test_data$Price, prediction = output$net.result)
 head(results)
 #Accuracy
+
+#REnormalize
 predicted=results$prediction * (Price_max_pre_normalize - Price_min_pre_normalize) + Price_min_pre_normalize
+
+
 actual=results$actual * (Price_max_pre_normalize - Price_min_pre_normalize) + Price_min_pre_normalize
+
 deviation=((actual-predicted)/actual)
 accuracy=1-abs(mean(deviation))
 accuracy
 
 ##Regression Tree Example 
+
 # Install the package
-install.packages("rpart")
 # Load the package
 library(rpart)
 
